3ff69ae62d37823b4d03be9883ae22a2ee7c08a2
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-03-01 18:12:54 +0100

    reeer

--- ConsoleSearch/App.cs
+++ ConsoleSearch/App.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
 
 namespace ConsoleSearch
 {
@@ -7,6 +11,8 @@ namespace ConsoleSearch
     {
         public void Run()
         {
+            HttpClient api = new HttpClient();
+            api.BaseAddress = new Uri("http://localhost:5171");
             //SearchLogic mSearchLogic = new SearchLogic(new Database());
             Console.WriteLine("Console Search");
             
@@ -16,6 +22,22 @@ namespace ConsoleSearch
                 string input = Console.ReadLine() ?? string.Empty;
                 if (input.Equals("q")) break;
 
+                Task<string> task = api.GetStringAsync("/Search?terms=" + input + "&numberOfResults=10");
+                task.Wait();
+                string resultString = task.Result;
+                SearchResult result = JsonConvert.DeserializeObject<SearchResult>(resultString);
+
+                foreach (var ignored in result.IgnoredTerms)
+                {
+                    Console.Write(ignored +  "was ignored");
+                }
+
+                foreach (var resltDocument in result.Documents)
+                {
+                    Console.WriteLine(resltDocument.Id + ": " + resltDocument.Path + " - number of terms found: " + resltDocument.NumberOfOccurrences);
+                }
+                
+                Console.WriteLine("Found " + result.Documents.Count + " in " + result.ElapsedMilliseconds + " ms");
 
             }
         }
