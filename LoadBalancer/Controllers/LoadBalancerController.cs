e15bd011783ddb5a71505925d3dce96049359eb7
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-03-06 22:02:34 +0100

    lebanon

--- LoadBalancer/Controllers/LoadBalancerController.cs
+++ LoadBalancer/Controllers/LoadBalancerController.cs
@@ -11,12 +11,13 @@ public class LoadBalancerController : ControllerBase
     private readonly ILoadBalancer _loadBalancer;
     
     [HttpGet] 
-    public async Task<SearchResult> Search(string terms, int numberOfResults)
+    public async Task<string> Search(string terms, int numberOfResults)
     {
         var nextService = _loadBalancer.NextService();
         HttpClient api = new HttpClient();
         api.BaseAddress = new Uri(nextService);
-        api.GetAsync("/Search?terms=" + terms + numberOfResults);
-        return null;
+        Task<string> task = api.GetStringAsync("/Search?terms=" + terms + "&numberOfResults=" + numberOfResults);
+        task.Wait();
+        return task.Result;
     }
 }
