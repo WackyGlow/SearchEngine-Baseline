973d572989d990dc17eba12a6c71b90ac46e5502
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2023-03-09 14:04:53 +0100

    Update Program.cs

--- SearchAPI/Program.cs
+++ SearchAPI/Program.cs
@@ -2,7 +2,7 @@ using RestSharp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var restClient = new RestClient("https://localhost:9001");
+var restClient = new RestClient("http://localhost:9001");
 restClient.Post(new RestRequest("configuration", Method.Post)
     .AddJsonBody(new
     {
