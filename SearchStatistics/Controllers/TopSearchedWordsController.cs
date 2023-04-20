97f81cb95c3af42d01587c116c6c437c71b7d283
Frede94 <frederikwinther@hotmail.dk>
2023-04-20 13:06:57 +0200

    Revert "removed searchstat"
    This reverts commit 761596c98698aa7771c6908dfaca542996b7b967.

---
+++ SearchStatistics/Controllers/TopSearchedWordsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchStatistics.Models;
+using SearchStatistics.Services.Implementations;
+
+namespace SearchStatistics.Controllers
+{
+    [ApiController]
+    [Route("words")]
+    public class TopSearchedWordsController : ControllerBase
+    {
+        TopSearchedWords tsw = new TopSearchedWords(Program.db);
+
+        [HttpGet("all")]
+        public List<SearchWord> GetAll()
+        {
+            return tsw.GetAll();
+        }
+
+        [HttpGet]
+        public List<SearchWord> Get([FromBody] GetModel getModel)
+        {
+            return tsw.Get(getModel.Amount, getModel.Order);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] PostModel postModel)
+        {
+            tsw.Post(postModel.Query);
+        }
+    }
+}
