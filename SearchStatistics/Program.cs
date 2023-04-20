46611e5be462199ffdc1cc37d71c80160742070b
Frede94 <frederikwinther@hotmail.dk>
2023-04-20 19:14:27 +0200

    Fixed SearchStatistic, added temp filepath to config

--- SearchStatistics/Program.cs
+++ SearchStatistics/Program.cs
@@ -1,29 +1,39 @@
 using SearchStatistics.DatabaseFolder;
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+public class Program
+{
+    public static Database db;
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        // Add services to the container.
 
-var app = builder.Build();
+        db = new Database();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-app.UseHttpsRedirection();
+        var app = builder.Build();
 
-app.UseAuthorization();
+        // Configure the HTTP request pipeline.
+        //if (app.Environment.IsDevelopment())
+        //{
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        //}
 
-app.MapControllers();
+        app.UseHttpsRedirection();
 
-app.Run();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
 
 
 /*
