7beb2f33894b331bffa2e3b75ee4db78496c6d0e
Frede94 <frederikwinther@hotmail.dk>
2023-04-26 16:47:26 +0200

    Update Config.cs

--- Common/Config.cs
+++ Common/Config.cs
@@ -2,13 +2,13 @@ namespace Common
 {
     public static class Config
     {
-        //public static string DatabasePath { get; } = "/data/database.db";
-        //public static string DataSourcePath { get; } = "/data/source";
-        //public static string SearchStatisticsDB { get; } = "/data/searchStats.db";
+        public static string DatabasePath { get; } = "/data/database.db";
+        public static string DataSourcePath { get; } = "/data/source";
+        public static string SearchStatisticsDB { get; } = "/data/searchStats.db";
 
-        public static string DatabasePath { get; } = "/DLS/database.db";
-        public static string DataSourcePath { get; } = "/DLS/source";
-        public static string SearchStatisticsDB { get; } = "/DLS/searchStats.db";
+        //public static string DatabasePath { get; } = "/DLS/database.db";
+        //public static string DataSourcePath { get; } = "/DLS/source";
+        //public static string SearchStatisticsDB { get; } = "/DLS/searchStats.db";
         public static int NumberOfFoldersToIndex { get; } = 10; // Use 0 or less for indexing all folders
     }
 }
